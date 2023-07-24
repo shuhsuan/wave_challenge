8ce3a911c7f26ad89453bd8e60ce763b035e489d
shuhsuan <shuhsuanchang.9@gmail.com>
2023-07-24 18:22:56 +0100

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,179 @@
+use serde::{Deserialize, Serialize};
+use std::clone::Clone;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
+use std::collections::HashMap;
 use std::fs;
-use std::fs::File;
-use serde_json::Value;
-use std::io::{BufWriter, Write};
-
-fn main() -> std::io::Result<()> {
-
-    //reading a file
-    let file = fs::File::open("simple_challenge_data.json")
-    .expect("file should open read only");
-
-    //parsing contents as a JSON
-    let json: serde_json::Value = serde_json::from_reader(file)
-    .expect("file should be JSON");
-
-    //extracting the list of 3D coordinates 
-    let _p = json.get("p")
-    .expect("file should have p key");
-
-    //extracting the list of quadrilaterals
-    let _q = json.get("q")
-    .expect("file should have q key");
-
-    //find the length of the array of q
-    let size_q = _q.as_array().unwrap().len();
-
-    let mut vec_pos: Vec<Value> = Vec::new();
-    let mut vec_neg: Vec<Value> = Vec::new();
-
-    //writing quadrilaterals which have a negative z-index directly to the file
-    let file_neg=File::create("wet_geometry.json")?;
-    let mut writer_neg = BufWriter::new(file_neg);
-    writer_neg.flush()?;
-
-    //writing quadrilaterals which have a positive z-index directly to the file
-    let file_pos=File::create("dry_geometry.json")?;
-    let mut writer_pos = BufWriter::new(file_pos);
-    writer_pos.flush()?;
-
-    let mut count_neg = 0;
-    let mut count_pos = 0;
 
 
-    for i in 0..size_q{  //for every quadrilateral of 4 points
+///This struct replicates the format of the given data
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    q: Vec<Quadrilateral>,
+    p: Vec<Point>,
+}
 
-        for x in 0..4{ //check each point for a negative z-index
-            
-            let coord_index = _q[i][x].as_u64().unwrap() as usize; //why does this not need a mut?
+///This struct type defines 3D coordinates 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Point([f64; 3]);
 
-            if _p[coord_index][2].as_f64() < Some(0.0){ //if the z-index of the p coordinate is negative
+///This struct type defines Quadrilaterals
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Quadrilateral([usize; 4]);
 
-                vec_neg.push(_q[i].clone()); 
-                count_neg+=1;
+
+impl Point {
+
+    ///This function returns the z-index
+    /// 
+    /// Example:
+    /// 
+    /// Set x to be a coordinate of type Point
+    ///```
+    /// let x: Point = [1, 2, -3];
+    /// ```
+    /// Find the z-index with the function
+    /// ```
+    /// let z_value = x.z(); 
+    /// ```
+    /// 
+    fn z(&self) -> f64 {
+        self.0[2]
+    }
+
+    ///This function checks if the z-index is below 0.0 and returns a boolean
+    /// 
+    /// Example:
+    /// 
+    /// Set x to be a coordinate of type Point
+    /// ```
+    /// let x: Point = [1, 2, -3];
+    /// ```
+    /// Use the function to check if the z-index is below 0.0
+    /// ```
+    /// let value = x.is_wet();
+    /// ```
+    /// The variable, value, will return true
+    fn is_wet(&self) -> bool {
+        self.z() < 0.0
+    }
+}
+
+///This function changes the value in the quadrilateral at the index given and returns the new quadrilateral
+/// 
+/// Example:
+/// 
+/// Given a quadrilateral 
+/// ```
+/// let mut q: Quadrilateral = [1,2,3,4];
+/// ```
+/// Change the 3rd value to 15
+/// ```
+/// q = change_q(q, 2, 15)
+/// ```
+/// The value of q is now
+/// ```
+/// [1,2,15,4]
+/// ```
+fn change_q(mut q: Quadrilateral, at_index: usize, new_value: usize) -> Quadrilateral {
+    q.0[at_index] = new_value;
+    q //return q
+}
+
+
+fn main() {
+    let data_string =
+        fs::read_to_string("./simple_challenge_data.json").expect("Unable to read file");
+    let data: Data = serde_json::from_str(&data_string).unwrap();
+
+    let q_data = data.q;
+    let p_data = data.p;
+
+    let mut count_neg: usize = 0;
+    let mut count_pos: usize = 0;
+
+    let mut map_neg: HashMap<usize, usize> = HashMap::new();
+    let mut map_pos: HashMap<usize, usize> = HashMap::new();
+
+    let mut p_neg: Vec<Point> = Vec::new();
+    let mut p_pos: Vec<Point> = Vec::new();
+
+    let mut q_negative: Vec<Quadrilateral> = Vec::new();
+    let mut q_positive: Vec<Quadrilateral> = Vec::new();
+
+    'loop_quads: for i in q_data {
+        //accesses q_data[i]
+
+        let index = i.0;
+
+        for a in index {
+            // a is an index intended to map to a point in p_data
+
+            if p_data[a].is_wet() {
+                //for negative z
+
+                match map_neg.entry(a) {
+                    Occupied(_) => {
+                        let position: usize = index.iter().position(|&r| r == a).unwrap(); // index of a in the quadrilteral
+                        let new_q_neg = change_q(i.clone(), position, map_neg[&a]); //change the index that maps to p
+                        q_negative.push(new_q_neg); //Push to a new array
+                    }
+
+                    Vacant(_) => {
+                        p_neg.push(p_data[a].clone()); //count and the index that p_data[a] is pushed into are the same
+                        let position: usize = index.iter().position(|&r| r == a).unwrap(); //the index value of the point
+                        let new_q_neg = change_q(i.clone(), position, count_neg); //change the index that maps to a new p
+                        q_negative.push(new_q_neg); //push to a new array
+                        count_neg += 1; //Increase count for next negative
+                    }
+                }
+
+                continue 'loop_quads; //I define the problem such that if just one p coordinate is wet then the quadrilateral is defined as wet and will be placed into the wet_data, thus it should skip to the next quadrilateral after it finds one wet coordinate
             }
-            else if _p[coord_index][2].as_f64() > Some(0.0){ //if the z-index of the p coordinate is positive
+            //for positive z
+            else {
+                let position: usize = index.iter().position(|&r| r == a).unwrap();
 
-                vec_pos.push(_q[i].clone());
-                count_pos+=1;
+                if position == 3 {
+                    //only place it in the dry_data if all points in the quadrilateral are found to be dry
+
+                    match map_pos.entry(a) {
+                        Occupied(_) => {
+                            let position: usize = index.iter().position(|&r| r == a).unwrap();
+                            let new_q_pos: Quadrilateral =
+                                change_q(i.clone(), position, map_pos[&a]);
+                            q_positive.push(new_q_pos);
+                        }
+                        Vacant(_) => {
+                            p_pos.push(p_data[a].clone());
+                            let position: usize = index.iter().position(|&r| r == a).unwrap();
+                            let new_q_pos: Quadrilateral = change_q(i.clone(), position, count_pos);
+                            q_positive.push(new_q_pos);
+                            count_pos += 1;
+                        }
+                    }
+                }
             }
         }
     }
 
-    serde_json::to_writer(&mut writer_neg, &vec_neg)?;
-    serde_json::to_writer(&mut writer_pos, &vec_pos)?;
+    let wet_data = Data {
+        q: q_negative,
+        p: p_neg,
+    };
 
+    let dry_data = Data {
+        q: q_positive,
+        p: p_pos,
+    };
 
-    println!("{}", count_neg);
-    println!("{}", count_pos);
-    println!("{}", size_q);
+    let wet_data_JSON = serde_json::to_string(&wet_data); //serializes the wet_data struct to a JSON string
+    let dry_data_JSON = serde_json::to_string(&dry_data); //serializes the dry_data struct to a JSON string
 
-    Ok(())
+    let mut f_wet = fs::File::create("wet_data.json").expect("Failed to create JSON file"); //creates a file called wet_data.json
+    let mut f_dry = fs::File::create("dry_data.json").expect("Failed to create JSON file"); //creates a file called dry_data.json
+
+    serde_json::to_writer(&mut f_wet, &wet_data); //serializes the wet_data struct to the wet_data.json file
+    serde_json::to_writer(&mut f_dry, &dry_data); //serializes the dry_data struct to the dry_data.json file
+
+    //The p points in wet_data and dry_data don't add up to p_data.len(), some values are repeated?
 }
-
-
